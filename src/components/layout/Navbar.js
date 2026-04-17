c651c1e01e6a150a6d4f906c58e5570c20427b7f
alissonrodri <alisson.rodrigo1307@gmail.com>
2026-04-16 21:41:16 -0300

    esboço navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+
+    const isAdmin = true;
+
+    return(
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            
+            <div className='navbar-logo'>
+                {/*incluir icone*/}
+                <span>CLT</span> Gaming
+            </div>
+            
+    
+        {/*No primeiro momento, essa barra deve sumir em mobile*/}
+            <div className='search-bar'>
+            <input type='text' placeholder='Buscar jogos...'/>
+            <span className='search-icon'>{/*incluir icone*/}P</span>
+            </div>
+
+        {/*Menu de navegação*/}
+            <div className='navbar-menu'>
+            <a href='#loja' className='navbar-item active'>Loja</a>
+            <a href='#biblioteca' className='navbar-iteme'>Biblioteca</a>
+            <a href='#rankings' className='navbar-item'>Rankings</a>
+            <a href='#painel' className='navbar-item'>Painel (ADM)</a> {/*Deve ser exibido apenas para conta com privilégio ADM*/}
+            </div>
+
+            <div className='navbar-actions'>
+                <div className='action-icon notification'>
+                    <span className='badge-notification'>2</span>
+                </div>
+
+                <div className='action-icon cart'>
+                    <span className='badge-cart'>3</span>
+                </div>
+
+                <div className='user-profile'>
+                    <span className='user-avatar'>AR</span>
+                </div>
+            </div>
+        </div>
+
+
+    </nav>
+    );
+}
+
+export default Navbar;
