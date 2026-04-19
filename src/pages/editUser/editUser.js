fd2915639f5a17ada57daf8b8491cc0b64d3a31e
Kaique Rios <kaiquerios.dev@gmail.com>
2026-04-19 11:20:26 -0300

    feat: teste da estrutura inicial da página editUser

---
+++ src/pages/editUser/editUser.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+function EditUser() {
+  return (
+    <div className="edit-user-overlay">
+      <div className="edit-user-container">
+        <h2 className="edit-user-title">Editar Perfil</h2>
+        <p className="edit-user-subtitle">teste</p>
+      </div>
+    </div>
+  );
+}
+
+export default EditUser;
