578b41cc38c5575dc915b046a6e91ab854c02c5f
Kaique Rios <kaiquerios.dev@gmail.com>
2026-04-19 12:12:23 -0300

    feat: estrutura e CSS base do editUser

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar/index';
+import EditUser from './pages/editUser/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Olá, CLT Gaming!</h1>
+      /*<EditUser />*/
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Navbar/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './index.css';
+
+function Navbar() {
+
+    const isAdmin = true;
+
+    return(
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            
+            <div className='navbar-logo'>
+                {/*incluir icone*/}
+                💼 <span>CLT</span> Gaming
+            </div>
+            
+    
+        {/*No primeiro momento, essa barra deve sumir em mobile*/}
+            <div className='search-bar'>
+            <input type='text' placeholder='Buscar jogos...'/>
+            <span className='search-icon'>{/*incluir icone*/}🔎</span>
+            </div>
+
+        {/*Menu de navegação*/}
+            <div className='navbar-menu'>
+            <a href='#loja' className='navbar-item active'>Loja</a>
+            <a href='#biblioteca' className='navbar-item'>Biblioteca</a>
+            <a href='#rankings' className='navbar-item'>Rankings</a>
+            </div>
+
+            </div>
+
+            <div className='navbar-actions'>
+
+                      {/*Deve ser exibido apenas para conta com privilégio ADM*/}
+                {isAdmin && (
+                <a href='#painel' className='navbar-item admin'>Painel ADM</a>
+                )}
+
+                <div className='action-icon notification'>
+                   🔔 <span className='badge-notification'>2</span>
+                </div>
+
+                <div className='action-icon cart'>
+                   🛒 <span className='badge-cart'>3</span>
+                </div>
+
+                <div className='user-profile'>
+                    <span className='user-avatar'>AR</span>
+                </div>
+            </div>
+    
+    </nav>
+    );
+}
+
+export default Navbar;
