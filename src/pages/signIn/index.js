7fa18f7a0f5ca5f33ea5126f131153bf1fe7bb61
gamadsantana <jvg.dsantana@gmail.com>
2026-04-21 17:01:25 -0300

    validação simples

--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -10,3 +10,8 @@ function SignIn() {
      /*função que lida com o envio do formulário*/
     const handleSubmit = (e) => {
         e.preventDefault(); //impede a página de recarregar
+//validação simples
+        if (!email || !password) {
+            setError('Por favor, preencha todos os campos.');
+            return;
+        }
