d268d3da08d5056ac462c3ab8933794c68501573
alissonrodri <alisson.rodrigo1307@gmail.com>
2026-04-21 18:23:26 -0300

    teste commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/home/index';
-/* import SignIn from './pages/signIn/index'; */
-/* import SignUp from './pages/signUp/index'; */
-/* import GamePage from './pages/gamePage/index'; */
+
 
 function App() {
   return (
     <div className="App">
       <HomePage/>
-      {/* <GamePage/> */}
+      
     </div>
   );
 }
