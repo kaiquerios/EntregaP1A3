8fdfdc56302a1faee73fad2c2b460ca8ac620f5d
alissonrodri <alisson.rodrigo1307@gmail.com>
2026-04-21 16:13:47 -0300

    inclusão de menu deslizante na navbar e inicio gamepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/home/index';
-
+/* import HomePage from './pages/home/index'; */
+import GamePage from './pages/gamePage/index';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      {/* <HomePage/> */}
+      <GamePage/>
     </div>
   );
 }
